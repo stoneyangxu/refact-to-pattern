9a43656c1178ba64439099f1ddeccecc51de58b2
stoneyangxu <stoneyangxu@icloud.com>
2017-08-23 19:53:20 +0800

    init tests

---
+++ src/test/scala/com/github/stoneyangxu/refacttopattern/MainSpec.scala
@@ -0,0 +1,12 @@
+package com.github.stoneyangxu.refacttopattern
+
+import org.scalatest.{FunSpec, Matchers}
+
+class MainSpec extends FunSpec with Matchers {
+
+  describe("Main") {
+    it("should run this test") {
+      true should be(true)
+    }
+  }
+}
